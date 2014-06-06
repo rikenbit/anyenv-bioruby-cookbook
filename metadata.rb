e70a9f78b35691122efadfdde68b74a68ccf1734
Manabu ISHII <manabu.ishii.rb@gmail.com>
2014-06-06 18:26:00 +0900

    install as user

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,2 @@
 name "anyenv-bioruby-cookbook"
-version "0.1.0"
+version "0.1.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,15 @@
 # libxml-ruby gem for BioRuby
 package "libxml2-dev"
 execute "install BioRuby" do
+  user "vagrant"
+  group "vagrant"
   command "export PATH=\"$HOME/.anyenv/bin:$PATH\";eval \"$(anyenv init -)\";gem install libxml-ruby"
   action :run
 end
 # install BioRuby
 execute "install BioRuby" do
+  user "vagrant"
+  group "vagrant"
   command "export PATH=\"$HOME/.anyenv/bin:$PATH\";eval \"$(anyenv init -)\";gem install bio"
   action :run
 end
